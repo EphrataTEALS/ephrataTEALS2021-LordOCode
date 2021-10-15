5b82639d625b2ceba7c66f656c0d4f4733d5b2e9
LordOCode <90467708+LordOCode@users.noreply.github.com>
2021-10-15 15:56:53 +0000

    yeet

---
+++ studentWork/EvenOdd.java
@@ -0,0 +1,15 @@
+package studentWork;
+
+public class EvenOdd {
+
+     static int x = -60000000;
+
+     public static void main (String[] args){
+         
+        if (x > 0) System.out.println(x + " is a positive number!");
+       else if (x == 0) System.out.println("You typed a zero!");
+        else System.out.println(x + " is a negative number");
+    }
+
+
+}
---
+++ studentWork/GoMetric.java
@@ -0,0 +1,16 @@
+package studentWork;
+
+public class GoMetric {
+     
+    public static void main(String[] args){
+    
+        double Fahrenheit;
+        Fahrenheit = 95;
+   
+        double Celsius;
+        Celsius = 35;
+  
+        System.out.println(Celsius+"Equals" + Fahrenheit);
+    }
+    
+}
