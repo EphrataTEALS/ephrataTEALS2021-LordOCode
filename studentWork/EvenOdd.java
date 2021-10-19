61ce3f900750e1c630ef52e3cfe2df64da6b6761
LordOCode <90467708+LordOCode@users.noreply.github.com>
2021-10-19 15:57:10 +0000

    Scanner

--- studentWork/EvenOdd.java
+++ studentWork/EvenOdd.java
@@ -2,9 +2,9 @@ package studentWork;
 
 public class EvenOdd {
 
-     static int x = -60000000;
+     static int x = 0;
 
-     public static void main (String[] args){
+     public static void main (String[] args) {
          
         if (x > 0) System.out.println(x + " is a positive number!");
        else if (x == 0) System.out.println("You typed a zero!");
---
+++ studentWork/TheScanner.java
@@ -0,0 +1,20 @@
+package studentWork;
+
+import java.util.*;
+
+public class TheScanner { 
+    public static void main(String[] args) {
+        Scanner Burrito = new Scanner(System.in);
+         
+        System.out.print("How old are you? ");
+        int Age = Burrito.nextInt();
+   
+   System.out.println(Age + " is your age!");
+  
+   Burrito.close();
+    }   
+
+        
+
+}
+    
