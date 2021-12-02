4e826619f50f08d970de40c159bee6caf9ef671d
LordOCode <90467708+LordOCode@users.noreply.github.com>
2021-12-02 16:56:18 +0000

    scond yeet

--- studentWork/Sampeee.java
+++ studentWork/Sampeee.java
@@ -16,13 +16,16 @@ public class Sampeee {
      for (int i = 0; i < numbersCount; i++) {
         System.out.println("What is the number you want to add?");
         int value = deagle.nextInt();
+        
+        if (value % 2 == 0){
         sum = sum + value;
+        }
 
       }
-
+      double average = sum / numbersCount;
       System.out.println("The sum is " + sum);
-  
-  
+      System.out.println("The average is " + average);
+      deagle.close();
     }
     
 }
