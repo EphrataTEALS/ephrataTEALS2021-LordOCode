5008e27fda175e4e4470829b5de990d8a3ff70d0
LordOCode <90467708+LordOCode@users.noreply.github.com>
2021-12-16 16:57:15 +0000

    yo

--- studentWork/GuessingGame.java
+++ studentWork/GuessingGame.java
@@ -7,13 +7,15 @@ package studentWork;
 
      public static void main(String[] args){
      
-     int turnCount = 7;
+     int turnCount = 6;
      Random r = new Random();
      int answer = r.nextInt(100);
         Scanner Guess = new Scanner(System.in);
-        while(turnCount >= 0){
-        System.out.println("Enter a number between 0 and 100");
-        int finalGuess = Guess.nextInt();
+          int stop = Guess.nextInt();
+        while(stop != -1 ){
+         while(turnCount >= 0){
+         System.out.println("Enter a number between 0 and 100");
+         int finalGuess = Guess.nextInt();
         if(finalGuess == answer){
           System.out.println("Nice! the answer is " + answer);
         }
@@ -24,9 +26,9 @@ package studentWork;
            System.out.println("Too Low");
           turnCount--;
           }
-
-      }
-         
-    }
-      
-  }
+        }
+        System.out.println("Thanks for playing!");
+      }    
+   }   
+  
+}
